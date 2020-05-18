22b7cccf98327f4ea9085d173a0a6be6c55e6bf7
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2020-05-18 13:47:52 +0100

    Create test client (#2)
    * AEA-142: Started writing a basic test client.
    
    * AEA-142: Populate request methods list automatically.
    
    * AEA-142: Refactored code which decides whether to include a request body.
    
    * AEA-142: Changed layout for clarity. Renamed variables and methods to conform to Javascript naming convention. Removed fake window buttons as they didn't do anything.
    
    * AEA-142: Moved style customisations into a stylesheet.
    
    * AEA-142: Build pipeline changes for test client.
    
    * Adding S3 bucket to host the client.
    
    * Updated default request URL to point to the Apigee proxy.
    
    * Removed CORS header as this is added by the Apigee proxy.
    
    * Changed build pipeline to not zip up the client files.
    
    Co-authored-by: Matt Jarvis <matt.jarvis@gmail.com>

---
+++ client/client.js
@@ -0,0 +1,104 @@
+const RequestMethods = Object.freeze({
+    GET: "GET",
+    POST: "POST"
+})
+
+function sendRequest() {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = handleResponse
+    const requestMethod = getRequestMethod();
+    xhr.open(requestMethod, getRequestUrl());
+    if (shouldIncludeRequestBody(requestMethod)) {
+        xhr.send(getRequestBody());
+    } else {
+        xhr.send(null);
+    }
+}
+
+function handleResponse() {
+    setResponseStatus(this.status + " " + this.statusText)
+    setResponseBody(this.responseText)
+    setResponseVisible(true);
+}
+
+function reset() {
+    setRequestUrl("https://internal-dev.api.service.nhs.uk/eps-steel-thread")
+    populateRequestMethodList()
+    requestMethodChanged()
+    setResponseBody("")
+    setResponseStatus("")
+    setResponseVisible(false)
+}
+
+function getRequestUrl() {
+    return document.getElementById("request-url").value
+}
+
+function setRequestUrl(value) {
+    document.getElementById("request-url").value = value
+}
+
+function getRequestMethod() {
+    return document.getElementById("request-method").value
+}
+
+function populateRequestMethodList() {
+    let request_method = document.getElementById("request-method");
+    request_method.options.length = 0
+    for (const method in RequestMethods) {
+        const option = document.createElement("option");
+        option.text = method
+        request_method.add(option)
+    }
+}
+
+function requestMethodChanged() {
+    setRequestBody("")
+    const include_request_body = shouldIncludeRequestBody(getRequestMethod());
+    setRequestBodyVisible(include_request_body)
+}
+
+function shouldIncludeRequestBody(requestMethod) {
+    switch (requestMethod) {
+        case RequestMethods.GET:
+            return false
+        case RequestMethods.POST:
+            return true
+        default:
+            console.error("Unhandled request method " + requestMethod)
+            return false
+    }
+}
+
+function getRequestBody() {
+    return document.getElementById("request-body").value
+}
+
+function setRequestBody(value) {
+    document.getElementById("request-body").value = value
+}
+
+function setRequestBodyVisible(value) {
+    setElementVisible("request-body-field-row", value)
+}
+
+function setResponseBody(value) {
+    document.getElementById("response-body").innerHTML = value
+}
+
+function setResponseStatus(value) {
+    document.getElementById("response-status").value = value
+}
+
+function setResponseVisible(value) {
+    setElementVisible("response", value)
+}
+
+function setElementVisible(elementId, visible) {
+    const field = document.getElementById(elementId)
+    if (visible) {
+        field.style.display = "block"
+    } else {
+        field.style.display = "none"
+    }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "project-name" {
   type    = string
   default = "eps-steel-thread"
 }
+
+variable "environment-name" {
+  type = string
+  default = "internal-dev"
+}
