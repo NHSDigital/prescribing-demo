87c8c41f70cc3dc319222b5dcc656395b4bc14f9
Matt Jarvis <matt.jarvis@gmail.com>
2020-05-31 19:45:39 +0100

    AEA-195: Updated FHIR message format: - Use PractitionerRole instead of Practitioner in most places. - Use MedicationRequest.groupIdentifier to store prescription ids. - Set Patient.generalPractitioner. - Set MedicationRequest.recorder.

--- client/static/example-prescriptions.js
+++ client/static/example-prescriptions.js
@@ -1,64 +1,64 @@
 let uuidNumber = 0
-function deterministicNextUuid()
-{
-  uuidNumber++
-  const uuidNumberStr = uuidNumber.toString(16)
-  const uuidNumberStrPadded = uuidNumberStr.padStart(32, "0")
-  return uuidNumberStrPadded.substring(0, 8) + "-"
-      + uuidNumberStrPadded.substring(8, 12) + "-"
-      + uuidNumberStrPadded.substring(12, 16) + "-"
-      + uuidNumberStrPadded.substring(16, 20) + "-"
-      + uuidNumberStrPadded.substring(20, 32)
+
+function deterministicNextUuid() {
+    uuidNumber++
+    const uuidNumberStr = uuidNumber.toString(16)
+    const uuidNumberStrPadded = uuidNumberStr.padStart(32, "0")
+    return uuidNumberStrPadded.substring(0, 8) + "-"
+        + uuidNumberStrPadded.substring(8, 12) + "-"
+        + uuidNumberStrPadded.substring(12, 16) + "-"
+        + uuidNumberStrPadded.substring(16, 20) + "-"
+        + uuidNumberStrPadded.substring(20, 32)
 }
 
 function CodeAndDesc(code, desc) {
-  this.code = code
-  this.desc = desc
+    this.code = code
+    this.desc = desc
 }
 
 function Coding(system, code, display) {
-  this.system = system
-  this.code = code
-  this.display = display
+    this.system = system
+    this.code = code
+    this.display = display
 }
 
 function Identifier(system, value) {
-  this.system = system
-  this.value = value
+    this.system = system
+    this.value = value
 }
 
 function Telecom(system, value, use) {
-  this.system = system
-  this.value = value
-  this.use = use
+    this.system = system
+    this.value = value
+    this.use = use
 }
 
 function Address(use, type, lines, city, postalCode) {
-  this.use = use
-  this.type = type
-  this.line = lines
-  this.city = city
-  this.postalcode = postalCode
+    this.use = use
+    this.type = type
+    this.line = lines
+    this.city = city
+    this.postalCode = postalCode
 }
 
 function Name(use, family, given, prefix, suffix) {
-  this.use = use
-  this.family = family
-  this.given = given
-  this.prefix = prefix
-  this.suffix = suffix
+    this.use = use
+    this.family = family
+    this.given = given
+    this.prefix = prefix
+    this.suffix = suffix
 }
 
 function Quantity(value, unitCode) {
-  this.value = value
-  this.unit = unitCode.desc
-  this.system = "http://snomed.info/sct"
-  this.code = unitCode.code
+    this.value = value
+    this.unit = unitCode.desc
+    this.system = "http://snomed.info/sct"
+    this.code = unitCode.code
 }
 
 function Entry(resource) {
-  this.resource = resource
-  this.fullUrl = "urn:uuid:" + this.resource.id
+    this.resource = resource
+    this.fullUrl = "urn:uuid:" + this.resource.id
 }
 
 /**
@@ -66,141 +66,176 @@ function Entry(resource) {
  */
 
 function Organization(odsCode, type, name) {
-  this.resourceType = "Organization"
-  this.id = deterministicNextUuid()
-  this.identifier = [
-      new Identifier("https://fhir.nhs.uk/Id/ods-organization-code", odsCode)
-  ]
-  this.type = [
-    {
-      coding: new Coding("urn:oid:2.16.840.1.113883.2.1.3.2.4.17.94", type.code, type.desc)
-    }
-  ]
-  this.name = name
+    this.resourceType = "Organization"
+    this.id = deterministicNextUuid()
+    this.identifier = [
+        new Identifier("https://fhir.nhs.uk/Id/ods-organization-code", odsCode)
+    ]
+    this.type = [
+        {
+            coding: [
+                new Coding("urn:oid:2.16.840.1.113883.2.1.3.2.4.17.94", type.code, type.desc)
+            ]
+        }
+    ]
+    this.name = name
 }
+
 Organization.prototype.addTelecom = function (telecom) {
-  if (!this.telecom) {
-    this.telecom = []
-  }
-  this.telecom.push(telecom)
+    if (!this.telecom) {
+        this.telecom = []
+    }
+    this.telecom.push(telecom)
 }
 Organization.prototype.addAddress = function (address) {
-  if (!this.address) {
-    this.address = []
-  }
-  this.address.push(address)
+    if (!this.address) {
+        this.address = []
+    }
+    this.address.push(address)
 }
 Organization.prototype.setParent = function (organization) {
-  this.partOf = {
-    reference: "urn:uuid:" + organization.id
-  }
+    this.partOf = {
+        reference: "urn:uuid:" + organization.id
+    }
 }
 
 function Encounter(organization) {
-  this.resourceType = "Encounter"
-  this.id = deterministicNextUuid()
-  this.status = "finished"
-  this.class = new Coding("http://terminology.hl7.org/CodeSystem/v3-ActCode", "AMB")
-  this.serviceProvider = {
-    reference: "urn:uuid:" + organization.id
-  }
+    this.resourceType = "Encounter"
+    this.id = deterministicNextUuid()
+    this.status = "finished"
+    this.class = new Coding("http://terminology.hl7.org/CodeSystem/v3-ActCode", "AMB")
+    this.serviceProvider = {
+        reference: "urn:uuid:" + organization.id
+    }
 }
 
 function Practitioner(sdsUserId, sdsJobRoleId, sdsRoleProfileId) {
-  this.resourceType = "Practitioner"
-  this.id = deterministicNextUuid()
-  this.identifier = [
-      new Identifier("https://fhir.nhs.uk/Id/sds-user-id", sdsUserId),
-      new Identifier("https://fhir.nhs.uk/Id/sds-job-role-id", sdsJobRoleId),
-      new Identifier("https://fhir.nhs.uk/Id/sds-role-profile-id", sdsRoleProfileId)
-  ]
+    this.resourceType = "Practitioner"
+    this.id = deterministicNextUuid()
+    this.identifier = [
+        new Identifier("https://fhir.nhs.uk/Id/sds-user-id", sdsUserId),
+        new Identifier("https://fhir.nhs.uk/Id/sds-job-role-id", sdsJobRoleId),
+        new Identifier("https://fhir.nhs.uk/Id/sds-role-profile-id", sdsRoleProfileId)
+    ]
 }
-Practitioner.prototype.addName = function(name) {
-  if (!this.name) {
-    this.name = []
-  }
-  this.name.push(name)
+
+Practitioner.prototype.addName = function (name) {
+    if (!this.name) {
+        this.name = []
+    }
+    this.name.push(name)
 }
-Practitioner.prototype.addTelecom = function(telecom) {
-  if (!this.telecom) {
-    this.telecom = []
-  }
-  this.telecom.push(telecom)
+Practitioner.prototype.addTelecom = function (telecom) {
+    if (!this.telecom) {
+        this.telecom = []
+    }
+    this.telecom.push(telecom)
+}
+
+function PractitionerRole(practitioner, organization) {
+    this.resourceType = "PractitionerRole"
+    this.id = deterministicNextUuid()
+    this.practitioner = {
+        reference: "urn:uuid:" + practitioner.id
+    }
+    this.organization = {
+        reference: "urn:uuid:" + organization.id
+    }
 }
 
 function Patient(nhsNumber, gender, dateOfBirth) {
-  this.resourceType = "Patient"
-  this.id = deterministicNextUuid()
-  this.identifier = [
-      new Identifier("https://fhir.nhs.uk/Id/nhs-number", nhsNumber)
-  ]
-  this.gender = gender
-  this.birthDate = dateOfBirth
+    this.resourceType = "Patient"
+    this.id = deterministicNextUuid()
+    this.identifier = [
+        new Identifier("https://fhir.nhs.uk/Id/nhs-number", nhsNumber)
+    ]
+    this.gender = gender
+    this.birthDate = dateOfBirth
 }
+
 Patient.prototype.addName = function (name) {
-  if (!this.name) {
-    this.name = []
-  }
-  this.name.push(name)
+    if (!this.name) {
+        this.name = []
+    }
+    this.name.push(name)
 }
 Patient.prototype.addAddress = function (address) {
-  if (!this.address) {
-    this.address = []
-  }
-  this.address.push(address)
+    if (!this.address) {
+        this.address = []
+    }
+    this.address.push(address)
+}
+Patient.prototype.setGeneralPractitioner = function (practitioner) {
+    this.generalPractitioner = {
+        reference: "urn:uuid:" + practitioner.id
+    }
 }
 
 function DosageInstruction(textualDose) {
-  this.text = textualDose
+    this.text = textualDose
 }
 
-function MedicationRequest(medicationCode, quantity, authoredOn) {
-  this.resourceType = "MedicationRequest"
-  this.id = deterministicNextUuid()
-  this.status = "active"
-  this.intent = "order"
-  this.medicationCodeableConcept = {
-    coding: [
-        new Coding("http://snomed.info/sct", medicationCode.code, medicationCode.desc)
+function MedicationRequest(prescriptionIdentifier, prescriptionShortFormIdentifier, medicationCode, quantity, authoredOn) {
+    this.resourceType = "MedicationRequest"
+    this.id = deterministicNextUuid()
+    this.groupIdentifier = [
+        new Identifier("urn:uuid", prescriptionIdentifier),
+        new Identifier("urn:oid:2.16.840.1.113883.2.1.3.2.4.18.8", prescriptionShortFormIdentifier)
     ]
-  }
-  this.dispenseRequest = {
-    quantity: quantity
-  }
-  this.authoredOn = authoredOn
+    this.category = {
+        coding: new Coding("urn:oid:2.16.840.1.113883.2.1.3.2.4.17.25", "0001")
+    }
+    this.status = "active"
+    this.intent = "order"
+    this.medicationCodeableConcept = {
+        coding: [
+            new Coding("http://snomed.info/sct", medicationCode.code, medicationCode.desc)
+        ]
+    }
+    this.dispenseRequest = {
+        quantity: quantity
+    }
+    this.authoredOn = authoredOn
 }
-MedicationRequest.prototype.addDosageInstruction = function(dosageInstruction) {
-  if (!this.dosageInstruction) {
-    this.dosageInstruction = []
-  }
-  this.dosageInstruction.push(dosageInstruction)
+
+MedicationRequest.prototype.addDosageInstruction = function (dosageInstruction) {
+    if (!this.dosageInstruction) {
+        this.dosageInstruction = []
+    }
+    this.dosageInstruction.push(dosageInstruction)
 }
 MedicationRequest.prototype.setPatient = function (patient) {
-  this.subject = {
-    reference: "urn:uuid:" + patient.id
-  }
+    this.subject = {
+        reference: "urn:uuid:" + patient.id
+    }
 }
 MedicationRequest.prototype.setEncounter = function (encounter) {
-  this.encounter = {
-    reference: "urn:uuid:" + encounter.id
-  }
+    this.encounter = {
+        reference: "urn:uuid:" + encounter.id
+    }
 }
-MedicationRequest.prototype.setRequester = function (practitioner) {
-  this.requester = {
-    reference: "urn:uuid:" + practitioner.id
-  }
+MedicationRequest.prototype.setRequester = function (practitionerRole) {
+    this.requester = {
+        reference: "urn:uuid:" + practitionerRole.id
+    }
+}
+MedicationRequest.prototype.setRecorder = function (practitionerRole) {
+    this.recorder = {
+        reference: "urn:uuid:" + practitionerRole.id
+    }
 }
 
 function Bundle() {
-  this.resourceType = "Bundle"
-  this.id = deterministicNextUuid()
-  this.type = "collection"
+    this.resourceType = "Bundle"
+    this.id = deterministicNextUuid()
+    this.type = "collection"
 }
+
 Bundle.prototype.addEntry = function (entry) {
-  if (!this.entry) {
-    this.entry = []
-  }
-  this.entry.push(entry)
+    if (!this.entry) {
+        this.entry = []
+    }
+    this.entry.push(entry)
 }
 
 /**
@@ -238,11 +273,19 @@ const practitioner1 = new Practitioner("345747307432", "R0260", "125686540025")
 practitioner1.addName(new Name("official", "Becond", null, ["Dr"]))
 practitioner1.addTelecom(new Telecom("phone", "tel:0161867785", "work"))
 
+const practitionerRole1 = new PractitionerRole(practitioner1, organization1a)
+
 const patient1 = new Patient("9900028562", "female", "1981-11-11")
 patient1.addName(new Name("official", "Willow", ["Julie", "Lisa"], ["Miss"]))
 patient1.addAddress(new Address("home", "both", ["2 Abagail street,"], "Manchester", "M12 2WS"))
+patient1.setGeneralPractitioner(practitionerRole1)
+
+const prescriptionIdentifier1 = deterministicNextUuid()
+const shortFormPrescriptionIdentifier1 = ""
 
 const medicationRequest1 = new MedicationRequest(
+    prescriptionIdentifier1,
+    shortFormPrescriptionIdentifier1,
     new CodeAndDesc("971711000001105", "Fluticasone 250micrograms/actuation inhaler CFC free 120 dose"),
     new Quantity(100, quantityCodeDose),
     "2008-02-21T14:22:00+00:00"
@@ -250,11 +293,13 @@ const medicationRequest1 = new MedicationRequest(
 medicationRequest1.addDosageInstruction(new DosageInstruction("Inhale when needed"))
 medicationRequest1.setPatient(patient1)
 medicationRequest1.setEncounter(encounter1)
-medicationRequest1.setRequester(practitioner1)
+medicationRequest1.setRequester(practitionerRole1)
+medicationRequest1.setRecorder(practitionerRole1)
 
 const EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM = new Bundle()
 EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM.addEntry(new Entry(medicationRequest1))
 EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM.addEntry(new Entry(patient1))
+EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM.addEntry(new Entry(practitionerRole1))
 EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM.addEntry(new Entry(practitioner1))
 EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM.addEntry(new Entry(encounter1))
 EXAMPLE_PRESCRIPTION_SINGLE_LINE_ITEM.addEntry(new Entry(organization1a))
@@ -281,11 +326,19 @@ const practitioner2 = new Practitioner("125686540025", "R0260", "934565838956")
 practitioner2.addName(new Name("official", "Hurst", null, ["Dr"]))
 practitioner2.addTelecom(new Telecom("phone", "tel:011327534256", "work"))
 
+const practitionerRole2 = new PractitionerRole(practitioner2, organization2a)
+
 const patient2 = new Patient("9900008464", "male", "1973-04-21")
 patient2.addName(new Name("official", "Anderson", ["Michael", "Jack"], ["Mr"]))
 patient2.addAddress(new Address("home", "both", ["1 Otley Road,"], "Leeds", "LS6 5RU"))
+patient2.setGeneralPractitioner(practitionerRole2)
+
+const prescriptionIdentifier2 = deterministicNextUuid()
+const shortFormPrescriptionIdentifier2 = ""
 
 const medicationRequest2a = new MedicationRequest(
+    prescriptionIdentifier2,
+    shortFormPrescriptionIdentifier2,
     new CodeAndDesc("317896006", "Digoxin 125 microgram oral tablet"),
     new Quantity(28, quantityCodeTablet),
     "2008-02-27T11:38:00+00:00"
@@ -293,9 +346,12 @@ const medicationRequest2a = new MedicationRequest(
 medicationRequest2a.addDosageInstruction(new DosageInstruction("1 tablet after breakfast"))
 medicationRequest2a.setPatient(patient2)
 medicationRequest2a.setEncounter(encounter2)
-medicationRequest2a.setRequester(practitioner2)
+medicationRequest2a.setRequester(practitionerRole2)
+medicationRequest2a.setRecorder(practitionerRole2)
 
 const medicationRequest2b = new MedicationRequest(
+    prescriptionIdentifier2,
+    shortFormPrescriptionIdentifier2,
     new CodeAndDesc("319775004", "Aspirin 75 mg oral tablet"),
     new Quantity(28, quantityCodeTablet),
     "2008-02-27T11:38:00+00:00"
@@ -303,9 +359,12 @@ const medicationRequest2b = new MedicationRequest(
 medicationRequest2b.addDosageInstruction(new DosageInstruction("1 tablet during breakfast"))
 medicationRequest2b.setPatient(patient2)
 medicationRequest2b.setEncounter(encounter2)
-medicationRequest2b.setRequester(practitioner2)
+medicationRequest2b.setRequester(practitionerRole2)
+medicationRequest2b.setRecorder(practitionerRole2)
 
 const medicationRequest2c = new MedicationRequest(
+    prescriptionIdentifier2,
+    shortFormPrescriptionIdentifier2,
     new CodeAndDesc("377145005", "Lorazepam 2 mg oral tablet"),
     new Quantity(84, quantityCodeTablet),
     "2008-02-27T11:38:00+00:00"
@@ -313,9 +372,12 @@ const medicationRequest2c = new MedicationRequest(
 medicationRequest2c.addDosageInstruction(new DosageInstruction("3 tablets before breakfast"))
 medicationRequest2c.setPatient(patient2)
 medicationRequest2c.setEncounter(encounter2)
-medicationRequest2c.setRequester(practitioner2)
+medicationRequest2c.setRequester(practitionerRole2)
+medicationRequest2c.setRecorder(practitionerRole2)
 
 const medicationRequest2d = new MedicationRequest(
+    prescriptionIdentifier2,
+    shortFormPrescriptionIdentifier2,
     new CodeAndDesc("421375004", "Citalopram (as citalopram hydrobromide) 40 mg orodispersible tablet"),
     new Quantity(56, quantityCodeTablet),
     "2008-02-27T11:38:00+00:00"
@@ -323,7 +385,8 @@ const medicationRequest2d = new MedicationRequest(
 medicationRequest2d.addDosageInstruction(new DosageInstruction("2 tablets after breakfast"))
 medicationRequest2d.setPatient(patient2)
 medicationRequest2d.setEncounter(encounter2)
-medicationRequest2d.setRequester(practitioner2)
+medicationRequest2d.setRequester(practitionerRole2)
+medicationRequest2d.setRecorder(practitionerRole2)
 
 const EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS = new Bundle()
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(medicationRequest2a))
@@ -331,6 +394,7 @@ EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(medicationRequest2b)
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(medicationRequest2c))
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(medicationRequest2d))
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(patient2))
+EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(practitionerRole2))
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(practitioner2))
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(encounter2))
 EXAMPLE_PRESCRIPTION_MULTIPLE_LINE_ITEMS.addEntry(new Entry(organization2a))
